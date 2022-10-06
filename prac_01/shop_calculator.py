1260b69b606e7b8c6e3380ee96087f5803416d97
brennen52 <brennen.caruana@my.jcu.edu.au>
2022-10-06 16:35:02 +1000

    Updated code

--- prac_01/shop_calculator.py
+++ prac_01/shop_calculator.py
@@ -1,14 +1,14 @@
 items = int(input("Number of Items: "))
-while True:
-    total = 0
-    if items >= 0:
-        for i in range(items):
-            cost = float(input("Price of Item: $"))
-            total = total + cost
-        if total > 100:
-            total = total * 0.9
+total = 0
+if items >= 0:
+    for i in range(items):
+        cost = float(input("Price of Item: $"))
+        total = total + cost
+    if total > 100:
+        total = total * 0.9
         print("Total price for", items, "items is ${:.2f}".format(total))
         items = int(input("Number of Items: "))
-    else:
+else:
+    while items < 0
         print("Invalid number of items! Please try again")
         items = int(input("Number of Items: "))
