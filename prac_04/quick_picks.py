1880ad3a2c6176c6ae098279e0db9b7fe050e62f
brennen52 <brennen.caruana@my.jcu.edu.au>
2022-10-12 22:11:29 +1000

    Added right alignment and 2 char long room for numbers

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,8 +1,9 @@
 import random
 
-number_of_quick_picks = int(input("How many quick picks would you like? "))
+number_of_quick_picks = int(input("How many quick picks? "))
 
 for n in range(number_of_quick_picks):
+    quick_pick_numbers = []
     for i in range(6):
-        print(random.randint(1, 45), end=" ")
+        print("{:2}".format(random.randint(1, 45)), end=" ")
     print("")
