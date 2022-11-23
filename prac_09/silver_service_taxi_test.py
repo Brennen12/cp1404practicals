87b275f107c9b1bb069dc2c0778a0ad949ff9f23
brennen52 <brennen.caruana@my.jcu.edu.au>
2022-11-23 21:10:33 +1000

    Update prac 9 taxi class to round fare to 10c

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -4,7 +4,7 @@ from prac_09.silver_service_taxi import SilverServiceTaxi
 def main():
     my_taxi = SilverServiceTaxi("Hummer", 200, 2)
     my_taxi.drive(18)
-    print(f"{my_taxi}, Current Fare = ${my_taxi.get_fare()}")
+    print(f"{my_taxi}, Current Fare = ${my_taxi.get_fare():.2f}")
 
 
 main()
