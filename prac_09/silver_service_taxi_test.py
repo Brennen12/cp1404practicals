b8e29f823a7af4a247a279f1cc7316a29e518e15
brennen52 <brennen.caruana@my.jcu.edu.au>
2022-11-23 21:02:44 +1000

    Complete prac 9 silver service taxi exercises

---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,10 @@
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    my_taxi = SilverServiceTaxi("Hummer", 200, 2)
+    my_taxi.drive(18)
+    print(f"{my_taxi}, Current Fare = ${my_taxi.get_fare()}")
+
+
+main()
